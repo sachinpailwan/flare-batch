3a07b9e9fc611b8b531bf9babb4393b69ea3a3a1
pailwan <sachinpailwan@gmail.com>
2022-01-11 22:30:37 +0530

    initia

---
+++ src/main/java/com/pailsom/flarebatch/FlareBatchApplication.java
@@ -0,0 +1,13 @@
+package com.pailsom.flarebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlareBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlareBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pailsom/flarebatch/FlareBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pailsom.flarebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlareBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
